7fb71e29d222236a1127c19fcd9b3040fc4dbf88
AnaCarolcs <anacarolinacsilva@outlook.com>
2019-09-06 12:25:11 -0300

    Lista 2

--- Lista2/631A.cpp
+++ Lista2/631A.cpp
@@ -1,23 +1,64 @@
 #include <bits/stdc++.h>
-//WIP
+
 using namespace std;
+
+int matriza[1000][1000], matrizb[1000][1000];
 
 int main(){
 
-    int n, a[1000], b[1000], soma;
+    int n, a[1000], b[1000], soma = 0, aux = 1;
 
     cin >> n;
-    
-    for (int i = 0; i < n; i++){
+
+    for (int i = 1; i <= n; i++){
         cin >> a[i];
     }
 
-    for (int i = 0; i < n; i++){
+    for (int i = 1; i <= n; i++){
         cin >> b[i];
     }
 
+    while(aux <= n){
+        matriza[aux][aux]=a[aux];
+        matrizb[aux][aux]=b[aux];
 
-    
+        for(int j=aux+1;j<=n;j++){
+            matriza[aux][j] = (matriza[aux][j-1]|(a[j]));
+            matrizb[aux][j] = (matrizb[aux][j-1]|(b[j]));
+
+            /*if(matriza[aux][j-1] && a[j]){
+                matriza[aux][j] = matriza[aux][j-1] + a[j];
+            }else if (matriza[aux][j-1]){
+                matriza[aux][j] = matriza[aux][j-1];
+            }else if(a[j]){
+                matriza[aux][j] = a[j];
+            } else {
+                matriza[aux][j];
+            }
+
+            if(matrizb[aux][j-1] && b[j]){
+                matrizb[aux][j] = matrizb[aux][j-1] + b[j];
+            }else if(matrizb[aux][j-1]){
+                matrizb[aux][j] = matrizb[aux][j-1];
+            }else if(b[j]){
+                matrizb[aux][j] = b[j];
+            } else {
+                matrizb[aux][j];
+            }*/
+        }
+        aux++;
+    }
+
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<=n;j++){
+            if(soma > (matriza[i][j]+matrizb[i][j])){
+                soma = soma;
+            }else{
+                soma = matriza[i][j]+matrizb[i][j];
+            }
+        }
+    }
+
     cout << soma << endl;
 
     return 0;
