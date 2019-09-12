296998262687b85f37cfde0c6e968c89f4467600
AnaCarolcs <anacarolinacsilva@outlook.com>
2019-09-12 13:56:46 -0300

    Questão 7 - Lista 3

--- Lista3/507A.cpp
+++ Lista3/507A.cpp
@@ -4,31 +4,42 @@ using namespace std;
 
 int main(){
 
-  int n, k, g = 0;
+    int n, k, val, aux = 0, count = 0, a=0;
 
-  cin >> n >> k;
+    cin >> n >> k;
 
-  vector<int> is(n);
-  vector<int> res(k);
+    pair<int, int> par;
+    vector<pair<int, int>> vetPar;
 
-  for (int i = 0; i < n; ++i){
-    cin >> is[i];
-  }
-
-  sort(is.begin(), is.end());
-
-  while(k){
-    if(is[g] <= k){
-      k -= is[g];
-      res[g] = is[g];
+    for(int i = 0; i < n; i++){
+        cin >> val;
+        par = make_pair(val, i);
+        vetPar.push_back(par);
+        if(vetPar[i].first > k){
+            aux++;
+        }
     }
-    g++;
-  }
 
-  for (int i = 0; res[i] != 0 ; ++i){
-    cout << res[i] << " ";
-  }
-    cout << endl;
+    sort(vetPar.begin(), vetPar.end());
+
+    if(aux >= n){
+        cout << "0";
+    } else {
+
+        for (int d = 0; d < n; d++){
+            if(vetPar[d].first <= k){
+                k -= vetPar[d].first;
+                count++;
+            }
+        }
+
+        cout << count << endl;
+
+        for (int i = 0; i < count; i++){
+            a = vetPar[i].second + 1;
+            cout << a << " ";
+        }
+    }
 
   return 0;
 }
