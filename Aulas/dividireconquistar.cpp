ff19d5bebe71e673f1f6848bbeb4624827388761
AnaCarolcs <anacarolinacsilva@outlook.com>
2019-10-07 15:44:02 -0300

    Exemplo de algoritmo dividir e conquistar

---
+++ Aulas/dividireconquistar.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(int a, int b){
+
+  return a + (b - a)/2;
+
+}
+
+int main(){
+
+  int a,b;
+  cin >> a >> b;
+
+  vector<int> as(n), bs(n);
+
+  cout << solve(a, b) << endl;
+
+  return 0;
+}
