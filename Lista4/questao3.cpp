bf502543bddb2c916efc0f33b767b82a71d5e2c9
AnaCarolcs <anacarolinacsilva@outlook.com>
2019-10-07 15:03:45 -0300

    Questão 03

--- Lista4/questao3.cpp
+++ Lista4/questao3.cpp
@@ -4,5 +4,26 @@ using namespace std;
 
 int main(){
 
+  string s[100];
+  int n, count = 1, res = 0;
+  cin >> n;
+
+  for(int i = 0; i < n; i++){
+        cin>>s[i];
+  }
+
+  sort(s, s+n);
+
+  for(int i = 1; i < n; i++){
+    if (s[i]!=s[i-1]){
+      res = max(count, res);
+      count=1;
+    }else{
+      count++;
+    }
+  }
+
+  cout << max(count, res) << endl;
+
   return 0;
 }
